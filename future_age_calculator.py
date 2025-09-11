6b1ae8859b65d447e014f596fc532853bbc2af0d
Hanna Araya Gebremedhin <hannagebremedhin945@gmail.com>
2025-09-11 15:41:03 +0300

    Add Python introduction tasks

---
+++ future_age_calculator.py
@@ -0,0 +1,8 @@
+# Ask user for input
+age = int(input("How old are you? "))
+
+# Calculate future age
+future_age = age + 27  # 2050 - 2023 = 27
+
+# Print result
+print(f"In 2050, you will be {future_age} years old.")
